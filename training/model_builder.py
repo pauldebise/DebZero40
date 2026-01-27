a25a7a8af031d8f182b1dee4ea3e956b59e86839
pauldebise <paul.debise@gmail.com>
2026-01-27 22:09:13 +0100

    Adding nets info

--- training/model_builder.py
+++ training/model_builder.py
@@ -89,6 +89,6 @@ def build_model(input_shape=(8, 8, 112), blocks=10, filters=128, se_ratio=8):
 
 
 if __name__ == "__main__":
-    model = build_model(input_shape=(8, 8, 24), blocks=5, filters=64, se_ratio=8)
+    model = build_model(input_shape=(8, 8, 12), blocks=15, filters=256, se_ratio=8)
 
     model.summary()
