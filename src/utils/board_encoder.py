5d9a989a46a8ba9b1f698f8c606e9dc9a13203dc
pauldebise <paul.debise@gmail.com>
2026-02-01 19:22:36 +0100

    Correcting point of view for board encoding

--- src/utils/board_encoder.py
+++ src/utils/board_encoder.py
@@ -1,25 +1,33 @@
-import chess
 import numpy as np
+import chess
 
 def fen_to_planes_int8(fen):
-
     board = chess.Board(fen)
 
-    planes = np.zeros((8, 8, 12), dtype=np.int8)
 
+    planes = np.zeros((8, 8, 12), dtype=np.int8)
     piece_map = board.piece_map()
+
+
+    is_black_turn = (board.turn == chess.BLACK)
 
     for square, piece in piece_map.items():
         rank = chess.square_rank(square)
         file = chess.square_file(square)
 
-        piece_offset = piece.piece_type - 1
-        color_offset = 0 if piece.color == chess.WHITE else 6
 
-        if board.turn == chess.BLACK:
-            color_offset = 1 - color_offset
+        if is_black_turn:
+            rank = 7 - rank
 
-        planes[rank, file, piece_offset + color_offset] = 1
+
+        piece_type_idx = piece.piece_type - 1
+        is_my_piece = (piece.color == board.turn)
+
+        if is_my_piece:
+            channel = piece_type_idx
+        else:
+            channel = piece_type_idx + 6
+
+        planes[rank, file, channel] = 1
 
     return planes
-
